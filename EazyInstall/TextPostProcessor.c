add3bbd8038303ed9e0cba97f8d5dc56e39b5f6c
Chika Ohaya <chika.ohaya@gmail.com>
2023-11-12 19:18:49 -0500

    Added postprocess function for quick text manipulation

---
+++ EazyInstall/TextPostProcessor.c
@@ -0,0 +1,8 @@
+//
+//  TextPostProcessor.c
+//  EazyInstall
+//
+//  Created by Chika Ohaya on 11/12/23.
+//
+
+#include "TextPostProcessor.h"
---
+++ EazyInstall/TextPostProcessor.h
@@ -0,0 +1,13 @@
+//
+//  TextPostProcessor.h
+//  EazyInstall
+//
+//  Created by Chika Ohaya on 11/12/23.
+//
+
+#ifndef TextPostProcessor_h
+#define TextPostProcessor_h
+
+#include <stdio.h>
+
+#endif /* TextPostProcessor_h */
